467c19174f67ae498ffa08d1b0f0b742c5fbe34f
Sabin Gautam <sabingt0@gmail.com>
2022-04-18 20:04:59 +0545

    menu has been added on topbar

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,22 @@
+import './Menu.scss'
+
+export default function Menu({menuOpen, setMenuOpen}) {
+  return (
+    <div className={"menu " +  (menuOpen && "active")}>
+        <ul>
+            <li>
+                <a href='#intro' onClick={()=>{setMenuOpen(!menuOpen)}}>Home</a>
+            </li>
+            <li>
+                <a href='#portfolio' onClick={()=>{setMenuOpen(!menuOpen)}}>Portfolio</a>
+            </li>
+            <li>
+                <a href='#works' onClick={()=>{setMenuOpen(!menuOpen)}}>Works</a>
+            </li>
+            <li>
+                <a href='#contact' onClick={()=>{setMenuOpen(!menuOpen)}}>Contact</a>
+            </li>
+        </ul>
+    </div>
+  )
+}
