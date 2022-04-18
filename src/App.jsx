a6b9b7d31a7c7b34aeb7c2552ce7695c51443b2f
Sabin Gautam <sabingt0@gmail.com>
2022-04-19 00:08:35 +0545

    Intro page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Contact from "./components/Contact/Contact";
+import Intro from "./components/Intro/Intro";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Topbar from "./components/Topbar/Topbar";
+import Works from "./components/Works/Works";
+import Menu from "./components/Menu/Menu";
+import './App.scss'
 
 function App() {
- 
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
     <div className="app">
-    Hello
+   <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+    <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+    <div className="sections">
+      <Intro />
+      <Portfolio />
+      <Works />
+      <Contact />
+    </div>
     </div>
   );
 }
