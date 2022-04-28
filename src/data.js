9232327c5cb14fc7842496bb7e6215dd4fe5aa45
Sabin Gautam <sabingt0@gmail.com>
2022-04-28 22:30:37 +0545

    portfolio cards added

---
+++ src/data.js
@@ -0,0 +1,65 @@
+export const featuredPortfolio = [
+  {
+    id: 1,
+    title: "Social Media App",
+    img: "https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900",
+  },
+  {
+    id: 2,
+    title: "Rampa UI Design",
+    img: "https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900",
+  },
+  {
+    id: 3,
+    title: "E-commerce Web Design",
+    img: "https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900",
+  },
+  {
+    id: 4,
+    title: "Relax Mobile App",
+    img: "https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900",
+  },
+  {
+    id: 5,
+    title: "Hero Web Design",
+    img: "https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900",
+  },
+  {
+    id: 6,
+    title: "Banking App",
+    img: "https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900",
+  },
+];
+
+export const webPortfolio = [
+  {
+    id: 1,
+    title: "Web Social Media App",
+    img: "https://cdn.dribbble.com/users/387658/screenshots/15458608/media/e92ec1dd33dd1170ae3dc9c5272f2693.jpg?compress=1&resize=1200x900",
+  },
+  {
+    id: 2,
+    title: "Web Rampa UI Design",
+    img: "https://cdn.dribbble.com/users/387658/screenshots/15458608/media/e92ec1dd33dd1170ae3dc9c5272f2693.jpg?compress=1&resize=1200x900",
+  },
+  {
+    id: 3,
+    title: "Web E-commerce Design",
+    img: "https://cdn.dribbble.com/users/387658/screenshots/15458608/media/e92ec1dd33dd1170ae3dc9c5272f2693.jpg?compress=1&resize=1200x900",
+  },
+  {
+    id: 4,
+    title: "Web Relax App",
+    img: "https://cdn.dribbble.com/users/387658/screenshots/15458608/media/e92ec1dd33dd1170ae3dc9c5272f2693.jpg?compress=1&resize=1200x900",
+  },
+  {
+    id: 5,
+    title: "Web Design",
+    img: "https://cdn.dribbble.com/users/387658/screenshots/15458608/media/e92ec1dd33dd1170ae3dc9c5272f2693.jpg?compress=1&resize=1200x900",
+  },
+  {
+    id: 6,
+    title: "Web Banking App",
+    img: "https://cdn.dribbble.com/users/387658/screenshots/15458608/media/e92ec1dd33dd1170ae3dc9c5272f2693.jpg?compress=1&resize=1200x900",
+  },
+];
