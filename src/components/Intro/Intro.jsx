ae2a0fcfc73a91a77b7814ec20a3d9c725573732
Sabin Gautam <sabingt0@gmail.com>
2022-04-29 01:30:02 +0545

    changes to intro page

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,52 +1,65 @@
 import "./Intro.scss";
-import person from "../../assets/person.png";
-import Typewriter from "typewriter-effect";
-import {
-  Facebook,
-  Instagram,
-  GitHub,
-  ArrowDownward,
-} from "@mui/icons-material";
-
+import { Facebook, Instagram, Twitter, LinkedIn } from "@mui/icons-material";
+import hero from "../../assets/hero.png";
 export default function Intro() {
   return (
-    <div className="intro" id="intro">
-      <div className="left">
-        <div className="imageContainer">
-          <img src={person} alt="" />
-        </div>
+    <div className="hero" id="intro">
+      <div className="container">
+      <div className="hero_heading">
+      <h1 className="hero__heading">
+          <span>Hello, This is</span>
+          <span className="hero__name">Sabin Gautam</span>
+        </h1>
       </div>
-      <div className="right">
-        <div className="wrapper">
-          <div className="introInfo">
-            <h2>Hello, It's me</h2>
-            <h1>Sabin Gautam</h1>
-            <h3>
-              I do<span>
-              <Typewriter
-            options={{
-              strings: ["Dveloping", "Designing", "Hosting "],
-              autoStart: true,
-              loop: true,
-            }}
-          />
-              </span>
-            </h3>
+
+        <div className="hero__img">
+          <img src={hero} alt="" />
+        </div>
+        <div className="hero__info">
+          <p className="paragraph">
+            I am working as a freelance web designer and developer for 4 years.
+            I love to design and make new web experiences for the people.
+          </p>
+          <button id="button1">About Me</button>
+        </div>
+        <div className="hero__social">
+         
+          <div className="hero__social__text">
+            <ul>
+              <li>
+                <a
+                  href="http://facebook.com/webcifar"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Facebook />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="http://twitter.com/webcifar"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Twitter />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="http://isntagram.com/web_cifar"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Instagram />
+                </a>
+              </li>
+              <li>
+                <a href="http://webcifar.com" target="_blank" rel="noreferrer">
+                  <LinkedIn />
+                </a>
+              </li>
+            </ul>
           </div>
-          <div className="IntroIcons">
-            <div className="icon">
-              <Facebook />
-            </div>
-            <div className="icon">
-              <Instagram />
-            </div>
-            <div className="icon">
-              <GitHub />
-            </div>
-          </div>
-          <a href="#portfolio">
-            <ArrowDownward className="downArrow" />
-          </a>
         </div>
       </div>
     </div>
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,5 +1,5 @@
 import './Menu.scss'
-import { Home, Article, ContactPage} from "@mui/icons-material";
+import { Home, Article, ContactPage, Analytics} from "@mui/icons-material";
 
 
 export default function Menu({menuOpen, setMenuOpen}) {
@@ -13,7 +13,7 @@ export default function Menu({menuOpen, setMenuOpen}) {
                 <a href='#portfolio' onClick={()=>{setMenuOpen(!menuOpen)}}><span><Article /></span>Portfolio</a>
             </li>
             <li>
-                <a href='#works' onClick={()=>{setMenuOpen(!menuOpen)}}>Works</a>
+                <a href='#works' onClick={()=>{setMenuOpen(!menuOpen)}}><Analytics/>Works</a>
             </li>
             <li>
                 <a href='#contact' onClick={()=>{setMenuOpen(!menuOpen)}}><span><ContactPage /></span>Contact</a>
